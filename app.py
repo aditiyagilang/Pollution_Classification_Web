6f912c2d9163ba28b55d9913cccdc639a25d6dff
AditiyaGilang <aditiyagilang031@gmail.com>
2024-07-21 13:16:31 +0700

    new_server

--- app.py
+++ app.py
@@ -1,52 +1,38 @@
+
 from flask import Flask, request, jsonify, render_template
-import mysql.connector
-from mysql.connector import Error
-
-app = Flask(__name__)
+from db import connect_db  # Import fungsi connect_db dari db.py
 
 
-db_config = {
-    'host': 'localhost',
-    'database': 'air_quality',
-    'user': 'root',
-    'password': ''  
-}
-
-def connect_db():
-    try:
-        connection = mysql.connector.connect(**db_config)
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    return None
-
-@app.route('/api/esp/data', methods=['GET'])
+@app.route('/api/esp/data', methods=['POST'])
 def receive_data():
-    esp_id = request.args.get('ESP-ID')
-    pm10 = request.args.get('PM10')
-    pm25 = request.args.get('PM25')
-    co = request.args.get('CO')
-    hc = request.args.get('HC')
-    ispu_pm10 = request.args.get('ISPU_PM10')
-    ispu_pm25 = request.args.get('ISPU_PM25')
-    ispu_co = request.args.get('ISPU_CO')
-    ispu_hc = request.args.get('ISPU_HC')
+    data = request.json
+    esp_id = data.get('ESP-ID')
+    pm10 = data.get('PM10')
+    pm25 = data.get('PM25')
+    co = data.get('CO')
+    hc = data.get('HC')
+    o3 = data.get('O3')  # Tambahkan data O3
+    ispu_pm10 = data.get('ISPU_PM10')
+    ispu_pm25 = data.get('ISPU_PM25')
+    ispu_co = data.get('ISPU_CO')
+    ispu_hc = data.get('ISPU_HC')
+    ispu_o3 = data.get('ISPU_O3')  # Tambahkan ISPU-O3
 
     connection = connect_db()
     if connection:
         cursor = connection.cursor()
         query = """
-            INSERT INTO sensor_data (esp_id, pm10, pm25, co, hc, ispu_pm10, ispu_pm25, ispu_co, ispu_hc)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO sensor_data (esp_id, pm10, pm25, co, hc, o3, ispu_pm10, ispu_pm25, ispu_co, ispu_hc, ispu_o3)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-        cursor.execute(query, (esp_id, pm10, pm25, co, hc, ispu_pm10, ispu_pm25, ispu_co, ispu_hc))
+        cursor.execute(query, (esp_id, pm10, pm25, co, hc, o3, ispu_pm10, ispu_pm25, ispu_co, ispu_hc, ispu_o3))
         connection.commit()
         cursor.close()
         connection.close()
         return jsonify({"status": "success"}), 200
     else:
         return jsonify({"status": "failed", "reason": "database connection error"}), 500
+  
    
 @app.route('/')
 def ispu_data():
@@ -64,4 +50,4 @@ def ispu_data():
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.2.12', port=5000)
