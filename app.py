e0083e01450391558efd5d5cdf5bd22201072e64
AditiyaGilang <aditiyagilang031@gmail.com>
2024-08-25 15:10:48 +0700

    server

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 from db import connect_db  
+from datetime import datetime
 
 app = Flask(__name__)
+
+
 
 @app.route('/api/esp/data', methods=['POST'])
 def receive_data():
@@ -17,17 +20,20 @@ def receive_data():
     ispu_co = data.get('ISPU_CO')
     ispu_hc = data.get('ISPU_HC')
     ispu_o3 = data.get('ISPU_O3')  
+    
+    # Mengambil waktu saat ini untuk timestamp
+    timestamp = datetime.now()
 
     connection = connect_db()
     if connection:
         cursor = connection.cursor()
         try:
-            # Insert into data_polusi table
+            # Insert into data_polusi table dengan timestamp
             insert_polusi_query = """
-                INSERT INTO data_polusi (esp_id, pm10, pm25, co, hc, o3)
-                VALUES (%s, %s, %s, %s, %s, %s)
+                INSERT INTO data_polusi (esp_id, pm10, pm25, co, hc, o3, timestamp)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
             """
-            cursor.execute(insert_polusi_query, (esp_id, pm10, pm25, co, hc, o3))
+            cursor.execute(insert_polusi_query, (esp_id, pm10, pm25, co, hc, o3, timestamp))
             polusi_id = cursor.lastrowid  # Get the last inserted id
 
             # Insert into ispu_data table
@@ -50,12 +56,17 @@ def receive_data():
     else:
         return jsonify({"status": "failed", "reason": "database connection error"}), 500
 
+
 @app.route('/')
 def ispu_data():
     connection = connect_db()
     if connection:
         cursor = connection.cursor(dictionary=True)
-        select_query = "SELECT * FROM data_polusi"
+        select_query = """
+            SELECT dp.*, ispu.ispu_pm10, ispu.ispu_pm25, ispu.ispu_co, ispu.ispu_hc, ispu.ispu_o3
+            FROM data_polusi dp
+            JOIN ispu_data ispu ON dp.data_id = ispu.polusi_id
+        """
         cursor.execute(select_query)
         records = cursor.fetchall()
         cursor.close()
@@ -63,6 +74,7 @@ def ispu_data():
         return render_template('index.html', data=records)
 
     return "Error connecting to the database", 500
-    
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8006)
--- db.py
+++ db.py
@@ -1,17 +1,28 @@
+import os
+from mysql.connector import connect, Error
 
-import mysql.connector
-from mysql.connector import Error
+def load_env_variables(env_path='.env'):
+    env_vars = {}
+    with open(env_path, 'r') as file:
+        for line in file:
+            if line.startswith('#') or not line.strip():
+                continue
+            key, value = line.strip().split('=', 1)
+            env_vars[key] = value
+    return env_vars
+
+env_vars = load_env_variables()
 
 db_config = {
-    'host': 'localhost',
-    'database': 'polusi_db',
-    'user': 'root',
-    'password': ''  
+    'host': env_vars.get('DATABASE_HOST'),
+    'database': env_vars.get('DATABASE_NAME'),
+    'user': env_vars.get('DATABASE_USER'),
+    'password': env_vars.get('DATABASE_PASSWORD')
 }
 
 def connect_db():
     try:
-        connection = mysql.connector.connect(**db_config)
+        connection = connect(**db_config)
         if connection.is_connected():
             return connection
     except Error as e:
